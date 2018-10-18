0dd6bf4efad3453298380b08d0e4093349e0138e
Dan Pastusek <dan@hexial.com>
2018-10-18 09:34:46 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,176 @@
+const URL = require('url').URL
+
+/* flow-include
+
+type simple = string
+
+type explicit = {
+  platform: string,
+  address: string,
+  name?: string,
+  email?: string,
+  weight?: number,
+  tier_currency?: string,
+  tiers?: Array<rewardTier>
+}
+
+type explicitMultiPlatform = {
+  platforms: Array<{
+    platform: string,
+    address: string
+  }>,
+  name?: string,
+  email?: string,
+  weight?: number,
+  tier_currency?: string,
+  tiers?: Array<rewardTiers>
+}
+
+type multiRecipient = {
+  recipients: Array<simple|explicit|explicitMultiPlatform>
+}
+
+type rewardTier = {
+  threshold: number,
+  name: string,
+  frequency: 'any'|'monthly'|'one-time',
+  description": string
+}
+
+export type donateSpecType = simple|explicit|multiRecipient|explicitMultiPlatform
+
+*/
+
+const platformMappings = {
+  'givethanks.app': 'Thanks',
+  'paypal.me': 'PayPal',
+  'patreon.com': 'Patreon'
+}
+
+function filterValidPlatform(platform /*: any */) /*: boolean */ {
+  return (
+    typeof platform === 'object' &&
+    typeof platform.platform === 'string' &&
+    typeof platform.address === 'string'
+  )
+}
+
+function parsePlatformFromString(string) {
+  try {
+    string = string.indexOf('://') > -1 ? string : 'https://' + string
+    const url = new URL(string)
+    return platformMappings[url.hostname] || 'Unknown'
+  } catch (err) {}
+}
+
+module.exports = function parse(data /*: any */) /*: specRecipientsType  */ {
+  // Handle "simple" type
+  if (typeof data === 'string') {
+    const platform = parsePlatformFromString(data)
+    if (!platform) {
+      return []
+    }
+    return [
+      {
+        weight: 1,
+        platforms: [{ platform, address: data }]
+      }
+    ]
+  }
+
+  if (typeof data !== 'object') return []
+
+  // Handle "explicit" type
+  if (typeof data.platform === 'string' && typeof data.address === 'string') {
+    return [
+      {
+        weight: 1,
+        name: data.name,
+        email: data.email,
+        platforms: [
+          {
+            platform: data.platform,
+            address: data.address
+          }
+        ]
+      }
+    ]
+  }
+
+  // Handle "explicitMultiPlatform" type
+  if (Array.isArray(data.platforms)) {
+    const platforms = data.platforms.filter(filterValidPlatform).map(({ platform, address }) => {
+      return { platform, address }
+    })
+    return [
+      {
+        weight: 1,
+        name: data.name,
+        email: data.email,
+        platforms
+      }
+    ]
+  }
+
+  // Handle "multiRecipient" type
+  if (Array.isArray(data.recipients)) {
+    const recipients = data.recipients
+      .map(recipient => {
+        if (typeof recipient === 'string') {
+          const platform = parsePlatformFromString(recipient)
+          if (!platform) {
+            return null
+          }
+          return [
+            {
+              weight: 1,
+              platforms: [{ platform, address: data }]
+            }
+          ]
+        }
+
+        const weight = recipient.weight ? parseFloat(recipient.weight) : 1
+
+        // Recipient has only a single platform
+        if (filterValidPlatform(recipient)) {
+          return {
+            weight,
+            name: recipient.name,
+            email: recipient.email,
+            platforms: [
+              {
+                platform: recipient.platform,
+                address: recipient.address
+              }
+            ]
+          }
+        }
+
+        // Recipient has multiple platforms
+        if (typeof recipient === 'object' && Array.isArray(recipient.platforms)) {
+          const platforms = recipient.platforms
+            .filter(filterValidPlatform)
+            .map(({ platform, address }) => {
+              return { platform, address }
+            })
+          return {
+            weight,
+            name: recipient.name,
+            email: recipient.email,
+            platforms
+          }
+        }
+
+        return null
+      })
+      .filter(recipient => recipient !== null)
+
+    //Normalize weights
+    const totalWeight = recipients.reduce((total, recipient) => total + recipient.weight)
+    recipients.forEach(recipient => (recipient.weight = recipient.weight / totalWeight))
+    return recipients
+  }
+
+  // Spec was invalid
+  return []
+}
---
+++ test.js
@@ -0,0 +1,98 @@
+const chai = require('chai')
+const parse = require('./index.js')
+const expect = chai.expect
+
+const describe = global.describe
+const it = global.it
+
+describe('parse()', () => {
+  it('parses simple specs!', () => {
+    const parsed = parse('givethanks.app/u/bobloblaw')
+    console.log({ parsed: parsed[0].platforms })
+    expect(Array.isArray(parsed)).to.equal(true)
+    expect(parsed[0]).to.be.a('object')
+    expect(Array.isArray(parsed[0].platforms))
+    expect(parsed[0].platforms.length, 'platforms.length').to.equal(1)
+  })
+
+  it('parses explicit specs!', () => {
+    const parsed = parse({
+      platform: 'foo',
+      address: 'bar'
+    })
+    expect(Array.isArray(parsed)).to.equal(true)
+    expect(parsed[0]).to.be.a('object')
+    expect(Array.isArray(parsed[0].platforms))
+    expect(parsed[0].platforms.length, 'platforms.length').to.equal(1)
+  })
+
+  it('parses explicitMultiPlatform specs!', () => {
+    const parsed = parse({
+      platforms: [
+        {
+          platform: 'foo2',
+          address: 'bar2'
+        }
+      ]
+    })
+    expect(Array.isArray(parsed)).to.equal(true)
+    expect(parsed[0]).to.be.a('object')
+    expect(Array.isArray(parsed[0].platforms))
+    expect(parsed[0].platforms.length, 'platforms.length').to.equal(1)
+  })
+
+  it('parses invalid spec (containing platforms & recipients)!', () => {
+    const parsed = parse({
+      platforms: [
+        {
+          platform: 'foo3',
+          address: 'bar3'
+        }
+      ],
+      recipients: [
+        {
+          platform: 'foo4',
+          address: 'bar4'
+        }
+      ]
+    })
+    expect(Array.isArray(parsed)).to.equal(true)
+    expect(parsed[0]).to.be.a('object')
+    expect(Array.isArray(parsed[0].platforms))
+    expect(parsed[0].platforms.length, 'platforms.length').to.equal(1)
+  })
+
+  it('parses invalid spec (number)!', () => {
+    const parsed = parse(25)
+    expect(Array.isArray(parsed)).to.equal(true)
+    expect(parsed.length).to.equal(0)
+  })
+
+  it('parses multiRecipient specs!', () => {
+    const emailTest = 'multi-recp-email@gmail.com'
+    const parsed = parse({
+      recipients: [
+        { platform: 'foo5', address: 'bar5' },
+        {
+          email: emailTest,
+          platforms: [
+            {
+              platform: 'foo6',
+              address: 'bar6'
+            },
+            {
+              platform: 'foo7',
+              address: 'bar7'
+            }
+          ]
+        }
+      ]
+    })
+    expect(Array.isArray(parsed)).to.equal(true)
+    expect(parsed[0]).to.be.a('object')
+    expect(Array.isArray(parsed[0].platforms))
+    expect(parsed[0].platforms.length, 'reply[0].platforms.length').to.equal(1)
+    expect(parsed[1].platforms.length, 'reply[1].platforms.length').to.equal(2)
+    expect(parsed[1].email, 'reply[1].email').to.equal(emailTest)
+  })
+})
